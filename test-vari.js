7efa4859fe07ad5ffe4fe769e6cb609c6ce47017
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-09-04 15:45:09 +0200

    [UPD] snack 3

--- test-vari.js
+++ test-vari.js
@@ -6,7 +6,18 @@ function createSlug(query) {
     return query.toLowerCase()
 }
 
+function average(arr, num) {
+    let sum;
+    arr.reduce((acc, curr) => {
+        return sum = acc + curr
+
+    }, 0)
+
+    return sum / num
+}
+
 module.exports = {
     getInitials,
-    createSlug
+    createSlug,
+    average
 }
--- test-vari.test.js
+++ test-vari.test.js
@@ -1,4 +1,4 @@
-const { getInitials, createSlug } = require('./test-vari.js')
+const { getInitials, createSlug, average } = require('./test-vari.js')
 
 // SNACK 1
 // Creare un test che verifichi la seguente descrizione:
@@ -9,10 +9,19 @@ test("La funzione getInitials restituisce le iniziali di un nome completo", () =
     expect(getInitials("Giulia")).toBe("G")
 })
 
-// SNACK2
+// SNACK 2
 // Creare un test che verifichi la seguente descrizione:
 // 👉 "La funzione createSlug restituisce una stringa in lowercase."
 
 test("La funzione createSlug restituisce una stringa in lowercase.", () => {
     expect(createSlug("JAVA")).toBe("java")
+})
+
+// SNACK 3
+// Creare un test che verifichi la seguente descrizione:
+// 👉 "La funzione average calcola la media aritmetica di un array di numeri."
+
+test("La funzione average calcola la media aritmetica di un array di numeri.", () => {
+    expect(average([4, 3, 5, 7, 1], 5)).toBe(4)
+    expect(average([5, 9, 7, 6, 10, 8, 4], 7)).toBe(7)
 })
